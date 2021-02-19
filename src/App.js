e3d6dad26af26c8fb5946987da118193b5caab79
juplmn777 <julien.plumyoen@gmail.com>
2021-02-19 09:56:00 +0100

    starting html done

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ function App() {
   return (
     <div className="container">
       <h1 className="title is-1 has-text-centered mt-5">Markdown-App</h1>
+
+      <div className="columns">
+        <div className="column is-half">
+          <div className="box">
+            <textarea
+              class="textarea is-danger has-fixed-size"
+              placeholder="Markdown Language"
+              rows="20"
+            ></textarea>
+          </div>
+        </div>
+        <div className="column is-half">
+          <div className="box">
+            <h2 className="title is-3 has-text-centered">Résultat html:</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
