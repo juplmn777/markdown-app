03b6589035c4dbbbaf7d06619e7d7f18b7bbad4b
juplmn777 <julien.plumyoen@gmail.com>
2021-02-20 10:01:32 +0100

    use  className='content' to render bulma style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import 'bulma/css/bulma.min.css';
+import marked from 'marked';
+import { sampleText } from './sampleText';
 
-function App() {
-  return (
-    <div className="container">
-      <h1 className="title is-1 has-text-centered mt-5">Markdown-App</h1>
+class App extends Component {
+  state = {
+    text: sampleText,
+  };
 
-      <div className="columns">
-        <div className="column is-half">
-          <div className="box">
-            <textarea
-              class="textarea is-danger has-fixed-size"
-              placeholder="Markdown Language"
-              rows="20"
-            ></textarea>
+  handleChange = (event) => {
+    const text = event.target.value;
+    this.setState({
+      text,
+    });
+  };
+
+  renderText = (text) => {
+    const markedOptions = {
+      sanitize: true,
+    };
+    const __html = marked(text, { markedOptions });
+    return { __html };
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <h1 className="title is-2 has-text-centered mt-2">Markdown-App</h1>
+
+        <div className="columns">
+          <div className="column is-half">
+            <div className="box">
+              <h2 className="title is-4 has-text-centered">MarkDown</h2>
+              <textarea
+                class="textarea is-danger has-fixed-size"
+                value={this.state.text}
+                onChange={this.handleChange}
+                rows="20"
+              ></textarea>
+            </div>
           </div>
-        </div>
-        <div className="column is-half">
-          <div className="box">
-            <h2 className="title is-3 has-text-centered">Résultat html:</h2>
+          <div className="column is-half">
+            <div className="box">
+              <h2 className="title is-4 has-text-centered">Résultat html:</h2>
+              <div className="content" dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
